a8a7eaf32a4e05b4a1b9fb89f0eb4effb19f7d6f
Sofia <sofiabravo103@gmail.com>
2013-03-14 20:30:37 -0430

    tom

--- tomar_tiempo.c
+++ tomar_tiempo.c
@@ -2,9 +2,10 @@
 #include <string.h>
 
 void main() {
-  FILE *f = popen("date +%s", "r");
+  FILE *f = popen("/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'", "r");
   char array[50];
   fgets(array, sizeof(array), f);
   printf("\nGot this: %s\n", array);
   pclose(f);
 }
+
